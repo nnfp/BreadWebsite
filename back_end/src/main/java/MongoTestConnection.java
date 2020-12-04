fdb18937791e5a0e1b6980f90fa370920e9bb375
nnfp <nnfpup@gmail.com>
2020-12-04 10:43:12 -0800

    created mongotestconnection

---
+++ back_end/src/main/java/MongoTestConnection.java
@@ -0,0 +1,22 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoTestConnection {
+
+    public static void main (String[]args){
+        MongoClient mongoClient = new MongoClient("localhost",27017);
+
+        MongoDatabase db = mongoClient.getDatabase("cs413finaldb");
+
+        MongoCollection<Document> usersCollection  = db.getCollection  ("listings");
+
+        Document doc = new Document("description :","test")
+                .append("type :","test")
+                .append("price :","test")
+                .append("title :","test");
+
+        usersCollection.insertOne(doc);
+    }
+}
