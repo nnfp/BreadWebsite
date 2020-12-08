97eeffb58ac3efed05b62f333461a005286f905b
nnfp <nnfpup@gmail.com>
2020-12-07 21:22:29 -0800

    editted css and js for app

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -102,63 +102,44 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Final Project App</h1>
-      <div key='xd'>
-          {/* <input value={message} onChange={e => setMessage(e.target.value)} placeholder="message"/> */}
-          <input id='input-description' value={desc} onChange={e => setDesc(e.target.value)} placeholder="Description">
-          </input>
-          <select id='input-type' value={type} onChange={e => setType(e.target.value)}>
-            <option value="" disabled selected>Bread Type</option>
-            <option value="bread 1">bread 1</option>
-            <option value="bread 2">bread 2</option>
-            <option value="bread 3">bread 3</option>
-            <option value="bread 4">bread 4</option>
-          </select>
-          <input id='input-price' value={price} onChange={e => setPrice(e.target.value)} placeholder="Price">
-          </input>
-          <input id='input-title' value={title} onChange={e => setTitle(e.target.value)} placeholder="Listing Title">
-          </input>
-          <input id='post-id' value={postId} onChange={e => setPostId(e.target.value)} placeholder="Post ID">
-          </input>
-          <select id='post-option' value={postOption} onChange={e => setPostOption(e.target.value)}>
-            <option value="" disabled selected>Post Option</option>
-            <option value="create">Create</option>
-            <option value="edit">Edit</option>
-            <option value="delete">Delete</option>
-          </select>
-          <button id='submit' onClick={handleClick}>
-            Submit
-          </button>
+      <div class="grid-container">
+        <div class ="grid-header" id="website-header">
+          <h1>Let's Get This Bread!</h1>
         </div>
-        <div key="kekw">
-          <h2>Messages:</h2>
-          {messages.map(item => <div key={item.id}>{item}</div>)}
+        <div class="grid-post">
+          <form class="grid-post-input">
+          <input value={message} onChange={e => setMessage(e.target.value)} placeholder="message"/>
+            <input id='input-description' value={desc} onChange={e => setDesc(e.target.value)} placeholder="Description">
+            </input>
+            <select id='input-type' value={type} onChange={e => setType(e.target.value)}>
+              <option value="" disabled selected>Bread Type</option>
+              <option value="bread 1">bread 1</option>
+              <option value="bread 2">bread 2</option>
+              <option value="bread 3">bread 3</option>
+              <option value="bread 4">bread 4</option>
+            </select>
+            <input id='input-price' value={price} onChange={e => setPrice(e.target.value)} placeholder="Price">
+            </input>
+            <input id='input-title' value={title} onChange={e => setTitle(e.target.value)} placeholder="Listing Title">
+            </input>
+            <input id='post-id' value={postId} onChange={e => setPostId(e.target.value)} placeholder="Post ID">
+            </input>
+            <select id='post-option' value={postOption} onChange={e => setPostOption(e.target.value)}>
+              <option value="" disabled selected>Post Option</option>
+              <option value="create">Create</option>
+              <option value="edit">Edit</option>
+              <option value="delete">Delete</option>
+            </select>
+          </form>
+          <button id='submit' class="grid-submit" onClick={handleClick}>
+              Submit
+            </button>
         </div>
-
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin</Link>
-          </li>
-          <li>
-            <Link to="/feed">Feed</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-        <Route path="/feed">
-          <Feed />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch> */}
+        <div class="grid-listing">
+          <h2>Listings:</h2>
+            {messages.map(item => <div key={item.id}>{item}</div>)}
+        </div>
+      </div>
     </div>
   );
 }
